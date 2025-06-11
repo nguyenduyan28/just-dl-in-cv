709bd0d18524a973a3e0bf78017d8f942d305664
nguyenduyan28 <nguyenduyan28@gmail.com>
2025-06-11 13:48:07 +0700

    Fix the arr layout, show image has label title

--- py_version/load_ds.py
+++ py_version/load_ds.py
@@ -30,6 +30,14 @@ def cifar10_get_labels():
 
 
 
+def reformat_arr(img_arr):
+  r = img_arr[: , : 1024].reshape(-1, 1)
+  g = img_arr[:, 1024 : 1024 * 2].reshape(-1, 1)
+  b = img_arr[:, 1024 * 2 :].reshape(-1, 1)
+  img_new = np.concatenate((r, g, b), axis = 1).reshape(img_arr.shape[0], -1)
+  return img_new
+
+
 def load_cifar10(val_split = False, return_label = False):
   X_train = [] 
   y_train = []
@@ -49,9 +57,9 @@ def load_cifar10(val_split = False, return_label = False):
         elif ("batches.meta" in file):
           label_names = [data.decode() for data in raw_data[b'label_names']]
 
-  X_train = np.concatenate(X_train).reshape(-1, 3072)
+  X_train = reformat_arr(np.concatenate(X_train).reshape(-1, 3072))
   y_train = np.array(y_train).reshape(-1, 1)
-  X_test = np.concatenate(X_test).reshape(-1, 3072)
+  X_test = reformat_arr(np.concatenate(X_test).reshape(-1, 3072))
   y_test = np.array(y_test).reshape(-1, 1)
   if val_split:
     X_val = X_train[-10000 :]
@@ -62,10 +70,11 @@ def load_cifar10(val_split = False, return_label = False):
   return (X_train), (y_train), (X_test), (y_test)
 
 
-def show_img(img_arr):
-  img_arr = img_arr.reshape(3, 32, 32).transpose(1, 2, 0) # transpose to switch C, H, W to W, H, C, reshape here because cifar10 rgb 
-  print(img_arr.dtype)
-  img_arr_normalized = np.clip(img_arr, 0, 255).astype(np.uint8) # if any < 0 --> 0, any > 255 --> 255
+def show_img(img_arr, label_arr):
+  img_arr_normalized = img_arr.reshape(32, 32, 3) # transpose to switch C, H, W to W, H, C, reshape here because cifar10 rgb 
+  label_list = cifar10_get_labels()
+  label_name = label_list[label_arr[0]]
+  plt.title(label_name)
   plt.imshow(img_arr_normalized) 
   plt.show()
 
@@ -74,7 +83,8 @@ def show_img(img_arr):
 
 
 
-
 if __name__ == '__main__':
   X_train, y_train, X_val, y_val, X_test, y_test = load_cifar10(val_split=True)
-  show_img(X_train[1])
+  print(X_train.shape)
+
+  show_img(X_val[0], y_val[0])
